2ed8e55d10349dd1d9e8e55d11f848bcc9d368e8
Pere Villega <pvillega@aracon.com>
2021-02-16 18:28:46 +0000

    Upgrade libraries, including upgrade of Tokio to 1.0
    Signed-off-by: Pere Villega <pvillega@aracon.com>

--- src/email_client.rs
+++ src/email_client.rs
@@ -173,7 +173,7 @@ mod tests {
         assert_err!(outcome);
     }
 
-    #[tokio::test(threaded_scheduler)]
+    #[tokio::test(flavor = "multi_thread")]
     async fn send_email_times_out_if_the_server_takes_too_long() {
         // Arrange
         let mock_server = MockServer::start().await;
